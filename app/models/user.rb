a37b26455fd7f637d70669a7be9c5c33f111382c
Lydia Scadding <lydiascadding@gmail.com>
2021-04-06 20:46:20 +0100

    Wrote validation for gh_username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,18 @@
+require 'json'
+require 'open-uri'
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :email. :password, :gh_username, presence: true
+  validates :email, :password, presence: true
+  validates :gh_username, presence: true, if: :username_exists?
+
+  def username_exists?
+    url = "https://api.github.com/users/#{gh_username}"
+    data = JSON.parse(URI.open(url).read)
+    errors.add(:gh_username, "must be a valid GitHub username") unless data["login"]
+  end
 end

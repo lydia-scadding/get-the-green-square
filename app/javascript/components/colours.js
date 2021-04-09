55613cabe9f55ebf4d7f679d0908b4d574e58e13
Lydia Scadding <lydiascadding@gmail.com>
2021-04-09 09:45:58 +0100

    Changed border to translucent outline to match gh styling

--- app/javascript/components/colours.js
+++ app/javascript/components/colours.js
@@ -1,4 +1,5 @@
-const colours = [ 'rgb(155,233,168)',
+const colours = [ 'rgb(235,237,240)',
+                  'rgb(155,233,168)',
                   'rgb(93,197,100)',
                   'rgb(75,162,79)',
                   'rgb(49,111,58)'
@@ -8,15 +9,14 @@ const counter = document.getElementById('push-count');
 
 const setColour = () => {
   const dataCount = parseInt(counter.dataset.count,10);
+  let idx;
   if (dataCount == 0) {
-    counter.style.backgroundColor = 'rgb(235,237,240)';
-    counter.style.borderColor = grey;
+    idx = 0;
   }
   else {
-    const idx = (dataCount / 7) >= 3 ? 3 : Math.floor(dataCount / 7);
-    counter.style.backgroundColor = colours[idx];
-    counter.style.borderColor = colours[idx];
+    idx = (dataCount / 7) >= 3 ? 4 : (Math.floor(dataCount / 7) + 1);
   }
+  counter.style.backgroundColor = colours[idx];
 }
 
 export { setColour };
